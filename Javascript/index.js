3d2d43ab22d6a0ed024556334dcce6efb350f168
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-04-12 12:38:25 +0200

    added local time and date

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,39 @@
+function updateTime() {
+  //Auckland
+  let AucklandElement = document.querySelector("#Auckland");
+  let AucklandDateElement = AucklandElement.querySelector(".date");
+  let AucklandTimeElement = AucklandElement.querySelector(".time");
 
+  let AucklandTime = moment().tz("NZ");
+
+  AucklandDateElement.innerHTML = AucklandTime.format("MMM Do YYYY");
+  AucklandTimeElement.innerHTML = AucklandTime.format(
+    "h:mm:ss[<small>] A [</small>]"
+  );
+
+  //Paris
+  let ParisElement = document.querySelector("#Paris");
+  let ParisDateElement = ParisElement.querySelector(".date");
+  let ParisTimeElement = ParisElement.querySelector(".time");
+
+  let ParisTime = moment().tz("Europe/Paris");
+
+  ParisDateElement.innerHTML = ParisTime.format("MMM Do YYYY");
+  ParisTimeElement.innerHTML = ParisTime.format(
+    "h:mm:ss[<small>] A [</small>]"
+  );
+
+  //Sweden
+  let StockholmElement = document.querySelector("#Stockholm");
+  let StockholmDateElement = StockholmElement.querySelector(".date");
+  let StockholmTimeElement = StockholmElement.querySelector(".time");
+
+  let StockholmTime = moment().tz("Europe/Stockholm");
+
+  StockholmDateElement.innerHTML = StockholmTime.format("MMM Do YYYY");
+  StockholmTimeElement.innerHTML = StockholmTime.format(
+    "h:mm:ss[<small>] A [</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
